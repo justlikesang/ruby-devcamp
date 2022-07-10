7ee649ad908784e672f43af79d4faf056837d933
Sang <sanghosuh@gmail.com>
2022-07-10 01:05:44 -0700

    Implement concern for managin image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,16 +1,18 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
   after_initialize :set_defaults
 
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
+  
   def self.angular
     where(subtitle: 'Angular')
   end
 
-  def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/350x200"
-  end
 end
 
 
